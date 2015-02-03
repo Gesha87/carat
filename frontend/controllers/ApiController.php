3fed6ae3d4c205a806857a0249cbaf9d961509e5
Andrey Kulik <kulik.andrei87@gmail.com>
2015-02-03 14:18:54 +0600

    apple crashes fix 2

--- frontend/controllers/ApiController.php
+++ frontend/controllers/ApiController.php
@@ -162,9 +162,7 @@ class ApiController extends Controller
 						unset($output);
 						if ($fileName) {
 							$fileName = Yii::getAlias('@app/data/dSYMs').'/'.$fileName;
-							var_dump($fileName);
-							exec("atosl -A $architecture -o \"$fileName\" -l $loadAddress $addresses", $output);
-							var_dump($output);
+							exec("atosl -A $architecture -o $fileName -l $loadAddress $addresses", $output);
 							if ($output) {
 								foreach ($output as $i => $line) {
 									$address = @$addressMatches[1][$i];
